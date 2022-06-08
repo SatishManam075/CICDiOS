8e358993be157e60d7a86caf149831a332008126
Satish Kumar Manam <manamsatishk@opentext.com>
2022-06-08 17:20:00 +0530

    Story board changes

--- FirstTestingApp/ViewController.swift
+++ FirstTestingApp/ViewController.swift
@@ -16,6 +16,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeBackgroundColor(_ sender: Any) {
+        self.view.backgroundColor = .random()
+    }
+    
 }
-
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(
+           red:   .random(),
+           green: .random(),
+           blue:  .random(),
+           alpha: 1.0
+        )
+    }
+}
